382cb3226d715f58ba2d61b825c9ab212d298b4d
Felix Kwan <kwfxng@gmail.com>
2019-11-03 00:44:34 +0800

    Add editorconfig

--- db-connect.js
+++ db-connect.js
@@ -3,9 +3,9 @@ const dbConnection = process.env.MONGODB_URI;
 
 mongoose.Promise = global.Promise;
 mongoose.connect(dbConnection).then(() => {
-    console.log('Connected to MongoDB.');
+  console.log('Connected to MongoDB.');
 }, (err) => {
-    console.log(err);
+  console.log(err);
 });
 
 module.exports = mongoose;
