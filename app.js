c7562070ab31fd8a15efe06fcf20639073c18077
Felix Kwan <kwfxng@gmail.com>
2019-11-03 12:31:57 +0800

    Update feedback and about feature

--- app.js
+++ app.js
@@ -1,26 +1,57 @@
 require('dotenv').config();
 const Telegraf = require('telegraf');
+const Extra = require('telegraf/extra');
 const _ = require('lodash');
 const { Contents } = require('./models');
 const SERVER_ERROR = '抱歉, 伺服器出現錯誤, 請稍候再試';
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
+bot.use(Telegraf.log());
 
-// Display Welcome Message
-bot.start((ctx) => {
+/* ---- Helper ---- */
+const displayContent = ({ reply }, key) => {
   Contents.find({})
     .then((contents) => {
       const content = _.head(contents);
-      ctx.reply(content.welcomeMessage);
+      reply(content[key]);
     })
     .catch(() => {
-      ctx.reply(SERVER_ERROR);
+      reply(SERVER_ERROR);
     });
+};
+
+const displayFeedbackLink = ({ reply }) => {
+  Contents.find({})
+    .then((contents) => {
+      const feedback = _.head(contents).feedback;
+      reply(feedback.text, Extra.markup((m) =>
+        m.inlineKeyboard([
+          m.urlButton(feedback.buttonLabel, feedback.link),
+        ])))
+    })
+    .catch(() => {
+      reply(SERVER_ERROR);
+    });
+};
+
+/* ---- Telegraf Action ---- */
+bot.start((ctx) => {
+  displayContent(ctx, 'welcomeMessage');
 });
 
+bot.help((ctx) => {
+  displayContent(ctx, 'welcomeMessage');
+});
 
-bot.help((ctx) => ctx.reply('Send me a sticker'));
-bot.on('sticker', (ctx) => ctx.reply('👍'));
-bot.hears('hi', (ctx) => ctx.reply('Hey there'));
+bot.command('feedback', (ctx) => {
+  displayFeedbackLink(ctx);
+});
+
+bot.help((ctx) => {
+  displayContent(ctx, 'about');
+});
+
+bot.on('message', (ctx) => ctx.reply('👍'));
+
 bot.launch();
 
--- schemas/contentSchema.js
+++ schemas/contentSchema.js
@@ -5,6 +5,24 @@ const contentSchema = new Schema({
   welcomeMessage: {
     type: Schema.Types.String,
     required: true
+  },
+  feedback: {
+    text: {
+      type: Schema.Types.String,
+      required: true
+    },
+    buttonLabel: {
+      type: Schema.Types.String,
+      required: true
+    },
+    link: {
+      type: Schema.Types.String,
+      required: true
+    }
+  },
+  about: {
+    type: Schema.Types.String,
+    require: true
   }
 });
 
